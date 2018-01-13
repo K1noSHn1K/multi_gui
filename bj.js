45121fa774ee50987f3a093c6dcb4c9514fb8f4c
K1noSHn1K <35163872+K1noSHn1K@users.noreply.github.com>
2018-01-13 12:07:18 +0300

    Update bj.js

--- bj.js
+++ bj.js
@@ -1,27 +1,21 @@
-var train1 = zavod = "", train2 = "", train3 = "", specialisation = "specialisationWestern", amSpecialisation = "western", compName = "", compName2 = "", walk1 = "foret", walk2 = "montagne", spec = "foret", KCK_Select = KCK_ALL, centerLocalisation = "centerLocalisationForet", nav = 0, mol = 360, name_male = "", name_female = "", offersToBeDone = 10, poroda = 38, shortPause1 = 70, shortPause2 = 100, mediumPause1 = 400, mediumPause2 = 500, longPause1 = 700, longPause2 = 800, centerCount = 2, centerPosition = 
-2;
-
+zavod = "";
+var train1 = "dressage", train2 = "endurance", train3 = "galop", specialisation = "specialisationWestern", amSpecialisation = "western", compName = "reining", compName2 = "", walk1 = "", walk2 = "", spec = "foret", KCK_Select = KCK_ALL, centerLocalisation = "centerLocalisationForet", nav = 0, mol = 360, name_male = "", name_female = "", offersToBeDone = 10, poroda = 38, shortPause1 = 70, shortPause2 = 100, mediumPause1 = 400, mediumPause2 = 500, longPause1 = 700, longPause2 = 800, centerCount = 2, 
+centerPosition = 2;
 load_settings();
-
 var HayToGive = 12, OatsToGive = 10;
 if ("undefined" !== typeof e1) {
   var enduranceTComplet = e1, vitesseTComplet = e2, dressageTComplet = e3, galopTComplet = e4, trotTComplet = e5, sautTComplet = e6, foretComplet = b3, montagneComplet = b1;
 }
-
 var genetics = ["galopGenetique"], lastParentPage = "", lastParentSex = "", offers = "BJ_offers", doAbort = "BJ_doAbort";
-
-console.log("Abort state = " + ReadLocalStorage(doAbort));
+console.log("Abort status = " + ReadLocalStorage(doAbort));
 console.log("Offers count = " + ReadLocalStorage(offers));
-
 void 0 !== ReadLocalStorage(doAbort) && null !== ReadLocalStorage(doAbort) || SaveLocalStorage(doAbort, !1);
 "" != ReadLocalStorage(offers) && void 0 !== ReadLocalStorage(offers) && null !== ReadLocalStorage(offers) || SaveLocalStorage(offers, 0);
 var amunitionEquiped = "amunitionEquiped" + getMyParameterByName("sautGenetique");
 if (window.self != window.top) {
   throw "stop";
 }
-
 var pause_reload = 25000;
-
 !1 === /www.lowadi.com\/elevage\/bureau/.test(window.location.href) && setTimeout(reload, pause_reload);
 /www.lowadi.com\/elevage\/chevaux\/\?elevage=all-horses/.test(window.location.href) && history.go(-2);
 if (/\/marche\/noir\/object\?qName=/.test(window.location.href)) {
@@ -30,12 +24,10 @@ if (/\/marche\/noir\/object\?qName=/.test(window.location.href)) {
   var pause1 = pause + getRandomPause(2 * longPause1, 2 * longPause2);
   setTimeout(doR, pause1);
 }
-
 if (/\/elevage\/chevaux\/cheval\?id=/.test(window.location.href)) {
   var sante = document.getElementById("sante").textContent;
   75 < sante && (6 >= chevalAge ? -1 == chevalNom.indexOf(".") ? (ClearLocalStorage(amunitionEquiped), giveHorseName()) : MainProg() : 6 < chevalAge && 16 >= chevalAge ? MainProg() : HorseTraining());
 }
-
 function MainProg() {
   function a() {
     if (/elevage\/chevaux\/centreInscription\?id=/.test(document.body.innerHTML)) {
@@ -72,7 +64,6 @@ function MainProg() {
   var g = getRandomPause(shortPause1, shortPause2);
   setTimeout(a, g);
 }
-
 function HorseTraining() {
   16 < chevalAge && 22 >= chevalAge ? (OatsToGive = 12, Walks(walk1)) : void 0 === document.getElementsByClassName("action action-style-4 competition-barrel action")[0] && void 0 === document.getElementsByClassName("action action-style-4 competition-galop action")[0] && 36 < chevalAge ? EquipAmunition() : 100 > getMyParameterByName(walk1 + "Complet") ? (OatsToGive = HayToGive = 14, Walks(walk1)) : 100 > getMyParameterByName(train1 + "TComplet") ? (OatsToGive = 14, Train(train1)) : 100 > getMyParameterByName(train2 + 
   "TComplet") ? (OatsToGive = 12, Train(train2)) : 60 < $(".competition-" + compName).attr("data-tooltip").length && 120 > chevalAge ? (console.log("\u0421\u043e\u0440\u0435\u0432\u043d\u043e\u0432\u0430\u043d\u0438\u044f"), HayToGive = -1, OatsToGive = 10, 25 < chevalEnergie && 1270 > timeConvert() ? (console.log("-=CompnoVIP=- Energy: " + chevalEnergie + " Time: " + timeConvert()), CompNoVIP()) : (console.log("-=ANOTHER=- Energy: " + chevalEnergie + " Time: " + timeConvert()), setTimeout(carrot, 
@@ -80,16 +71,13 @@ function HorseTraining() {
     MainProg();
   }, 400)) : (30 > chevalEnergie && (setTimeout(carrot, 100), setTimeout(minEnergy, 400), setTimeout(mash(), 700), setTimeout(sleep, 1000), setTimeout(OR, 1100)), "feminin" == chevalSexe ? BirthProg() : "masculin" == chevalSexe && parseInt(ReadLocalStorage(offers), 10) < offersToBeDone ? GiveSluchki(5) : "masculin" == chevalSexe && parseInt(ReadLocalStorage(offers), 10) >= offersToBeDone && (ClearLocalStorage(offers), GoToMother()));
 }
-
 function doR() {
   document.getElementsByClassName("button button-style-2")[0].click();
 }
-
 function st() {
   var a = document.getElementById("inventaire-utilisation-standard");
   null !== a && a.click();
 }
-
 function GiveSluchki(a) {
   function b() {
     var a = document.getElementsByClassName("action action-style-4 panser action-disabled"), d = document.getElementsByClassName("action action-style-4 panser-rainbow action-disabled");
@@ -156,12 +144,10 @@ function GiveSluchki(a) {
   var n = getRandomPause(shortPause1, shortPause2);
   setTimeout(b, n);
 }
-
 function GoToMother() {
   var a = document.getElementsByClassName("horsename")[1].href;
   document.location.replace("http://www.lowadi.com/elevage/chevaux/cheval?id=" + a.substring(a.search("=") + 1));
 }
-
 function giveOfferForTeam() {
   var a = 0 + getRandomPause(mediumPause1, mediumPause2);
   setTimeout(openReproduction, a);
@@ -249,7 +235,7 @@ function timeConvert() {
 }
 /www.lowadi.com\/elevage\/chevaux\/choisirNoms\?jument=/.test(window.location.href) && (-1 !== document.body.innerHTML.indexOf("femelle.png") ? document.getElementById("poulain-1").setAttribute("value", "\u041a\u043e\u0431\u044b\u043b\u0430") : document.getElementById("poulain-1").setAttribute("value", "\u0416\u0435\u0440\u0435\u0431\u0435\u0446"), setTimeout(function() {
   $("#boutonChoisirNom").click();
-}, 300));
+}, 700));
 /www.lowadi.com\/elevage\/chevaux\/saillie\?offre=/.test(window.location.href) && (pause1 = getRandomPause(mediumPause1, mediumPause2), pause = pause1 + getRandomPause(2 * mediumPause1, 2 * mediumPause2), setTimeout(function() {
   document.getElementById("boutonDoReproduction").click();
 }, pause));
@@ -577,12 +563,12 @@ function checkComp() {
 }
 function competitionByName(a) {
   console.log("competitionByName" + chevalEnergie);
-  a = document.getElementsByClassName("action action-style-4 competition-" + a);
+  a = 1 > document.getElementsByClassName("action action-style-4 competition-" + a + "-class-rainbow").length ? document.getElementsByClassName("action action-style-4 competition-" + a) : document.getElementsByClassName("action action-style-4 competition-" + a + "-class-rainbow");
   document.location.replace(a.item(0).href);
 }
 function competitionByNameVIP(a) {
   console.log("competitionByNameVIP" + chevalEnergie);
-  document.getElementsByClassName("action action-style-4 competition-" + a).item(0).click();
+  (1 > document.getElementsByClassName("action action-style-4 competition-" + a + "-class-rainbow").length ? document.getElementsByClassName("action action-style-4 competition-" + a).item(0) : document.getElementsByClassName("action action-style-4 competition-" + a + "-class-rainbow").item(0)).click();
 }
 function CompNoVIP() {
   function a() {
@@ -912,7 +898,7 @@ function KCK2() {
 }
 $("body").append('<div class="lwm_logo" style="display:block; z-index:9999; position:fixed; right:0;  top:120px; width:150px;"><img src="https://raw.githubusercontent.com/Crasher69/lowadi/master/horse.png" /></div>');
 $("body").append('<style>.lw {color:#fff;}</style><div class="lw_gui" style="display:block; z-index:9999; position:fixed; width:240px; height:auto; right:0; top:300px; padding:5px; background-color: rgba(0, 0, 0, 0.65);  border-radius: 10px 0px 0px 10px;"></div>');
-$(".lw_gui").append('<center><p style="color:#fff;"><span style="font-family: Arial,Helvetica,sans-serif; font-size: 12px; color:#F1F9F1;"><b>BotJack 0.4.8</b></span></p> </center>');
+$(".lw_gui").append('<center><p style="color:#fff;"><span style="font-family: Arial,Helvetica,sans-serif; font-size: 12px; color:#F1F9F1;"><b>BotJack 0.4.8b</b></span></p> </center>');
 $(".lw_gui").append('<center><p style="color:#fff;"><span style="font-family: Arial,Helvetica,sans-serif; font-size: 11px; color:#F1F9F1;">  </p></center> <hr>');
 $(".lw_gui").append('<span class="lw">id \u0437\u0430\u0432\u043e\u0434\u0430:</span> <input type="text" name="lw_zavod" id="lw_zavod" size="18"><br>');
 $(".lw_gui").append('<span class="lw">\u0422\u0440\u0435\u043d\u0438\u0440\u043e\u0432\u043a\u0430 1: &nbsp; &nbsp;</span> <select id="train1" name="train1">\t<option value="dressage">\u0432\u044b\u0435\u0437\u0434\u043a\u0430</option>\t<option value="galop">\u0433\u0430\u043b\u043e\u043f</option>\t<option value="saut">\u043f\u0440\u044b\u0436\u043a\u0438</option>\t<option value="trot">\u0440\u044b\u0441\u044c</option>\t<option value="endurance">\u0432\u044b\u043d\u043e\u0441\u043b\u0438\u0432\u043e\u0441\u0442\u044c</option>\t<option value="vitesse">\u0441\u043a\u043e\u0440\u043e\u0441\u0442\u044c</option>\t\t</select><br>');
